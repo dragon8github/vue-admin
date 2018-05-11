c22e5b7a5821382bc2dbc9aa33584f6676894166
dragon8github <928532756@qq.com>
2018-05-11 17:49:41 +0800

    cookie添加

--- src/utils/http.js
+++ src/utils/http.js
@@ -29,9 +29,9 @@ const http = axios.create({
  */
 http.interceptors.request.use(request => {
   /**
-   * 请不要尝试优化将此 request.headers['Authorization'] 的设置加入到移动到公共配置中去初始化。这会出现 Vue.cookie 不存在的错误。
-   * 因为在main.js中import总是提前执行（无论import放在什么位置），也就是说，【import http from './utils/http.js'】 总是比 【Vue.use(VueCookie)】执行的快。
-   * 这会导致Vue.cookie对象不存在，而又因为import的时候会执行axios.create中的代码块，估无法使用，
+   * 请不要尝试将此 request.headers['Authorization'] 的header设置移动到上面的【axios.create】公共配置中去。因为这会导致 Vue.cookie 不存在的错误。
+   * 原因是在 main.js 中 import 总是提前执行（无论 import 放在什么位置），也就是说 【import http from './utils/http.js'】 总是比 【Vue.use(VueCookie)】执行的快。
+   * 而又因为import的时候会执行【axios.create】中的代码块，这时候Vue.cookie还未被初始化，估无法会出现Vue.cookie对象不存在的错误。
    * 除非你将 【import http from './utils/http.js'】 改为 【var http = require('./utils/http.js').default】
    * 但这样未免太不优雅。所以我的解决方案是延迟执行（懒执行），在请求的时候才执行【Vue.cookie.get('token')】，那时候早就执行Vue.use(VueCookie)了。
    */
