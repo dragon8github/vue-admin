36a4fe5e2754534c535af7590b59853340d91b45
dragon8github <928532756@qq.com>
2018-05-13 13:48:08 +0800

    剩下400和重复的问题

--- src/utils/http.js
+++ src/utils/http.js
@@ -13,75 +13,90 @@ const UAT = 'http://172.16.200.112:30111/'
 const PRO = 'http://172.16.200.112:30111/'
 
 /**
- * 公共配置
+ * axios 配置
  */
 const http = axios.create({
-  timeout: 1000 * 10,
-  baseURL: process.env.NODE_ENV === 'production' ? UAT : DEV,
-  headers: {
-      'app': 'ALMS',
-      'X-Requested-With': 'XMLHttpRequest',
-      'Content-Type': 'application/json; charset=utf-8',
-  }
+    timeout: 1000 * 10,
+    baseURL: process.env.NODE_ENV === 'production' ? SIT : DEV,
+    headers: {
+        'app': 'ALMS',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json; charset=utf-8',
+    }
 })
+
+/**
+ * 获取并更新 refreshToken
+ */
+const getRefreshToken = () => {
+    // 从cookie中获取refreshToken
+    var refreshToken = Vue.cookie.get('refreshToken')    
+    // 如果refreshToken失效则重新登录，否则重置一下过期时间
+    if (refreshToken) {
+        // 重置为30分钟
+        Vue.cookie.set('refreshToken', refreshToken, { expires: '30m' });      
+    // 否则跳转登录
+    } else {
+        // 跳转到登录页
+        router.push('/login')
+        // 取消请求（我还不如直接throw呢）
+        throw new Error('登录超时，请重新登录。')
+    }
+    return refreshToken;
+}
+
+/**
+ * 获取 token
+ */
+const getToken = (refreshToken) => {
+    // 从cookie中获取token
+    var token = Vue.cookie.get('token')
+    // 如果token失效，则重新获取
+    if (!token || token == 'undefined' || token == 'null') {
+        return http.get('/uaa/auth/token').then(data => {
+            // 如果有token，说明获取成功
+            if (data.token) {
+                // 重置token
+                Vue.cookie.set('token', data.token, { expires: '28m' });
+                // 返回token
+                return data.token
+            } else {
+                // 取消请求
+                throw new Error(data.result_msg)
+            }
+        }).catch(err => {
+            // 跳转到登录页
+            router.push('/login')
+            // 取消请求
+            throw new Error('重新获取token失败，请重新登录。' + err.message)
+        })
+    } else {
+        return token
+    }
+}
+
 
 /**
  * 请求拦截
  */
 http.interceptors.request.use(async request => {
-    if ((['/uaa/auth/login', '/uaa/auth/token']).indexOf(request.url) < 0) {
-        // 从cookie中获取refreshToken
-        var refreshToken = Vue.cookie.get('refreshToken')
-        // 从cookie中获取token
-        var token = Vue.cookie.get('token')
+    // 如果是登录是不需要任何Authorization的
+    if ('/uaa/auth/login' === request.url) {
+        return request
 
-        // 如果refreshToken失效则重新登录，否则重置一下过期时间
-        if (refreshToken) {
-            // 重置为30分钟
-            Vue.cookie.set('refreshToken', refreshToken, { expires: '30m' });      
-        // 否则跳转登录
-        } else {
-            // 跳转到登录页
-            router.push('/login')
-            // 取消请求（我还不如直接throw呢）
-            throw new Error('登录超时，请重新登录。')
-        }
+    // 如果是重新获取token，Authorization需要设置为refreshToken
+    } else if ('/uaa/auth/token' === request.url) {
+        var refreshToken = getRefreshToken();
+        request.headers['Authorization'] = 'Bearer ' + refreshToken
+        return request
 
-        // 如果token失效，则重新获取
-        if (!token || token === 'undefined') {
-            // 重新请求获取最新的 token
-            token = await http.get('/uaa/auth/token', {
-                headers: {
-                  // 注意这里是将 Authorization 设置为 refreshToken
-                  Authorization: 'Bearer ' + refreshToken
-                }
-            }).then(data => {
-                console.log("获取到的最新token", data);
-                if (data.token) {
-                    // 重置token
-                    Vue.cookie.set('token', data.token, { expires: '28m' });
-                    // 返回一下token
-                    return data.token
-                  } else {
-                      // 跳转到登录页
-                      router.push('/login')
-                      // 取消请求
-                      throw new Error(data.result_msg)
-                  }
-            }).catch(err => {
-                // 跳转到登录页
-                router.push('/login')
-                // 取消请求
-                throw new Error('重新获取token失败，请重新登录。' + err.message)
-            })
-        }
-
-        // 所有请求都要带上授权码        
+    // 其余API的Authorization全部使用token
+    } else {
+        var refreshToken = getRefreshToken();
+        var token = await getToken(refreshToken);
         request.headers['Authorization'] = 'Bearer ' + token
+        return request;
     }
-    
-    // 一切正常，照常请求
-    return request
 }, err => {
   return Promise.reject(err)
 })
